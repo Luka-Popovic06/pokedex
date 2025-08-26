f052404b00ff3b6fb008896c093204399720b8b8
Luka Popovic <lukapopovic0111@gmail.com>
2025-08-26 22:52:37 +0200

    Add input listener to store Pokemon name or ID

--- input.js
+++ input.js
@@ -1,1 +1,5 @@
-
+import { domElements } from './dom.js';
+export let pokemonNameOrId;
+domElements.input.addEventListener('input', function (e) {
+  pokemonNameOrId = e.target.value.toLowerCase();
+});
