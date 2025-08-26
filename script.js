af1aac2c94af5b72fdd79ac11771269684ffe806
Luka Popovic <lukapopovic0111@gmail.com>
2025-08-26 22:48:45 +0200

    Add overlay click and form submit event listeners

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 'use strict';
+import { domElements } from './dom.js';
+import { getPokemon } from './pokemonCreator.js';
+
+domElements.main.addEventListener('click', function (e) {
+  if (e.target.closest('.overlay')) {
+    domElements.pokemonInfoCard.classList.add('hidden');
+    domElements.overlay.classList.add('hidden');
+  }
+});
+
+domElements.form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  getPokemon();
+});
