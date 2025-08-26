ec7c84ca2c1a85bbfb56bd4147ed35440eaed467
Luka Popovic <lukapopovic0111@gmail.com>
2025-08-26 22:54:02 +0200

    Add domElements object to centralize DOM queries

--- dom.js
+++ dom.js
@@ -1,1 +1,14 @@
-
+export const domElements = {
+  input: document.querySelector('.search-pokemon-input'),
+  form: document.querySelector('.search-pokemon-form'),
+  pokemonList: document.querySelector('.pokemon-list'),
+  pokemonInfoCard: document.querySelector('.selected-pokemon'),
+  pokemonImg: document.querySelector('.pokemon-image'),
+  pokemonName: document.querySelector('.pokemon-name'),
+  pokemonHeight: document.querySelector('.pokemon-height'),
+  pokemonWeight: document.querySelector('.pokemon-weight'),
+  pokemonType: document.querySelector('.pokemon-type'),
+  pokemonId: document.querySelector('.pokemon-id'),
+  overlay: document.querySelector('.overlay'),
+  main: document.querySelector('.container'),
+};
